afdebfa4da6e0d84d14cd46d89e0a0ba66276dc3
Peter Buelow <peter.buelow@lifefitness.com>
2019-04-26 19:12:19 -0500

    Initial commit
    * Created a basic UI
    * Added GPIO handling

--- src/robobrew.cpp
+++ src/robobrew.cpp
@@ -1,11 +1,219 @@
+/**
+ * RoboBrew
+ * robobrew.cpp
+ * Copyright Peter Buelow (goballstate@gmail.com)
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2019 Peter Buelow
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include "robobrew.h"
-#include "ui_robobrew.h"
 
-robobrew::robobrew(QWidget *parent) :
-    QMainWindow(parent),
-    m_ui(new Ui::robobrew)
+RoboBrew::RoboBrew(QWidget *parent) : QWidget(parent)
 {
-    m_ui->setupUi(this);
+    m_timeElapsed = 0;
+    m_timeElapsedSegment = 0;
+    
+    m_layout = new QGridLayout();
+    m_temp1 = new QLCDNumber(this);
+    m_temp2 = new QLCDNumber(this);
+    m_temp1Label = new QLabel(this);
+    m_temp2Label = new QLabel(this);
+    m_timeElapsedLabel = new QLCDNumber(this);
+    m_timeElapsedSegmentLabel = new QLCDNumber(this);
+    m_innerCoil = new QPushButton(this);
+    m_outerCoil = new QPushButton(this);
+    m_pump = new QPushButton(this);
+    m_starter = new QPushButton(this);
+    m_timerElapsedSegmentTime = new QTimer(this);
+    m_timerElapsedTime = new QTimer(this);
+    
+    m_temp1->setMinimumSize(((800 / 2) - 5), 100);
+    m_temp2->setMinimumSize(((800 / 2) - 5), 100);
+    m_timeElapsedLabel->setMinimumSize(((800 / 2) - 5), 50);
+    m_timeElapsedSegmentLabel->setMinimumSize(((800 / 2) - 5), 50);
+    m_temp1Label->setMinimumSize(((800 / 2) - 5), 50);
+    m_temp2Label->setMinimumSize(((800 / 2) - 5), 50);
+    m_innerCoil->setMinimumSize(((800 / 4) - 5), 150);
+    m_outerCoil->setMinimumSize(((800 / 4) - 5), 150);
+    m_pump->setMinimumSize(((800 / 4) - 5), 150);
+    m_starter->setMinimumSize(((800 / 4) - 5), 150);
+    
+    m_layout->addWidget(m_timeElapsedLabel, 0, 0, 1, 2, Qt::AlignCenter);
+    m_layout->addWidget(m_timeElapsedSegmentLabel, 0, 2, 1, 2, Qt::AlignCenter);
+    m_layout->addWidget(m_temp1Label, 1, 0, 1, 2, Qt::AlignCenter);
+    m_layout->addWidget(m_temp2Label, 1, 2, 1, 2, Qt::AlignCenter);
+    m_layout->addWidget(m_temp1, 2, 0, 1, 2, Qt::AlignCenter);
+    m_layout->addWidget(m_temp2, 2, 2, 1, 2, Qt::AlignCenter);
+    m_layout->addWidget(m_starter, 3, 0, Qt::AlignCenter);
+    m_layout->addWidget(m_innerCoil, 3, 1, Qt::AlignCenter);
+    m_layout->addWidget(m_outerCoil, 3, 2, Qt::AlignCenter);
+    m_layout->addWidget(m_pump, 3, 3, Qt::AlignCenter);
+    
+    setLayout(m_layout);
+    
+    m_innerCoil->setText("500 Watt");
+    m_outerCoil->setText("1000 Watt");
+    m_pump->setText("Pump");
+    m_starter->setText("Begin");
+    m_temp1Label->setText("Base Temperature");
+    m_temp2Label->setText("Floating Temperature");
+    m_timeElapsedLabel->display("0:00");
+    m_timeElapsedSegmentLabel->display("0:00");
+
+    QPalette pal = m_starter->palette();
+    pal.setColor(QPalette::Button, QColor(Qt::green));
+    m_starter->setAutoFillBackground(true);
+    m_starter->setPalette(pal);
+
+    m_relays = new RelayManager();
+
+    connect(m_innerCoil, SIGNAL(released()), this, SLOT(toggleInnerCoil()));
+    connect(m_outerCoil, SIGNAL(released()), this, SLOT(toggleOuterCoil()));
+    connect(m_pump, SIGNAL(released()), this, SLOT(togglePump()));
+    connect(m_starter, SIGNAL(released()), this, SLOT(startBrewing()));
+    connect(m_timerElapsedTime, SIGNAL(timeout()), this, SLOT(elapsedTimeout()));
+    connect(m_timerElapsedSegmentTime, SIGNAL(timeout()), this, SLOT(elapsedSegmentTimeout()));
+    connect(m_relays, SIGNAL(relayStateChanged(int, bool)), this, SLOT(relayStateChanged(int, bool)));
 }
 
-robobrew::~robobrew() = default;
+RoboBrew::~RoboBrew() = default;
+
+void RoboBrew::showEvent(QShowEvent*)
+{
+    bool state;
+    for (int i = 0; i < 3; i++) {
+        if (m_relays->getRelayState(i, &state))
+            relayStateChanged(i, state);
+        else
+            relayStateChanged(i, false);
+    }
+}
+
+void RoboBrew::toggleInnerCoil()
+{
+    bool state;
+    
+    if (m_relays->getRelayState(RM_INNER_COIL, &state))
+        m_relays->setRelayState(RM_INNER_COIL, !state);
+}
+
+void RoboBrew::toggleOuterCoil()
+{
+    bool state;
+    
+    if (m_relays->getRelayState(RM_OUTER_COIL, &state))
+        m_relays->setRelayState(RM_OUTER_COIL, !state);
+}
+
+void RoboBrew::togglePump()
+{
+    bool state;
+    
+    if (m_relays->getRelayState(RM_PUMP, &state))
+        m_relays->setRelayState(RM_PUMP, !state);
+}
+
+void RoboBrew::elapsedSegmentTimeout()
+{
+}
+
+void RoboBrew::elapsedTimeout()
+{
+    m_timeElapsed++;
+    int hour = m_timeElapsed / 60;
+    int minute = m_timeElapsed % 60;
+    QString displayValue = QString("%1:%2").arg(hour).arg(minute, 1, 10, QChar('0'));
+    m_timeElapsedLabel->display(displayValue);
+}
+
+void RoboBrew::setInnerCoilButtonBackground(bool state)
+{
+    QPalette pal = m_innerCoil->palette();
+    if (state)
+         pal.setColor(QPalette::Button, QColor(Qt::green));
+     else
+         pal.setColor(QPalette::Button, QColor(Qt::red));
+     m_innerCoil->setAutoFillBackground(true);
+    m_innerCoil->setPalette(pal);
+}
+
+void RoboBrew::setOuterCoilButtonBackground(bool state)
+{
+    QPalette pal = m_outerCoil->palette();
+    if (state)
+        pal.setColor(QPalette::Button, QColor(Qt::green));
+    else
+        pal.setColor(QPalette::Button, QColor(Qt::red));
+    m_outerCoil->setAutoFillBackground(true);
+    m_outerCoil->setPalette(pal);
+}
+
+void RoboBrew::setPumpButtonBackground(bool state)
+{
+    QPalette pal = m_pump->palette();
+    if (state)
+        pal.setColor(QPalette::Button, QColor(Qt::green));
+    else
+        pal.setColor(QPalette::Button, QColor(Qt::red));
+    m_pump->setAutoFillBackground(true);
+    m_pump->setPalette(pal);
+}
+
+void RoboBrew::relayStateChanged(int button, bool state)
+{
+    switch (button) {
+        case RM_INNER_COIL:
+            setInnerCoilButtonBackground(state);
+            break;
+        case RM_OUTER_COIL:
+            setOuterCoilButtonBackground(state);
+            break;
+        case RM_PUMP:
+            setPumpButtonBackground(state);
+            break;
+        default:
+            break;
+    }
+}
+
+void RoboBrew::startBrewing()
+{
+    m_timerElapsedTime->setInterval(RB_ONE_MINUTE);
+    m_timerElapsedTime->start();
+    QPalette pal = m_starter->palette();
+    pal.setColor(QPalette::Button, QColor(Qt::red));
+    m_starter->setAutoFillBackground(true);
+    m_starter->setPalette(pal);
+    m_starter->setText("End");
+}
+
+void RoboBrew::updateBaseTemp(double t)
+{
+    QString temp = QString("%1").arg(t, 0, 'f', 1);
+    m_temp1->display(temp);
+}
+
+void RoboBrew::updateFloatTemp(double t)
+{
+    QString temp = QString("%1").arg(t, 0, 'f', 1);
+    m_temp2->display(temp);
+}
+
--- src/robobrew.h
+++ src/robobrew.h
@@ -1,23 +1,83 @@
+/**
+ * RoboBrew
+ * robobrew.h
+ * Copyright Peter Buelow (goballstate@gmail.com)
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2019 Peter Buelow
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #ifndef ROBOBREW_H
 #define ROBOBREW_H
 
-#include <QMainWindow>
-#include <QScopedPointer>
+#include <QtCore/QtCore>
+#include <QtWidgets/QtWidgets>
 
-namespace Ui {
-class robobrew;
-}
+#include "relaymanager.h"
 
-class robobrew : public QMainWindow
+#define RB_ONE_MINUTE       (1000 * 60)
+
+class RoboBrew : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit robobrew(QWidget *parent = nullptr);
-    ~robobrew() override;
+    explicit RoboBrew(QWidget *parent = nullptr);
+    ~RoboBrew() override;
 
+public slots:
+    void toggleInnerCoil();
+    void toggleOuterCoil();
+    void togglePump();
+    void elapsedTimeout();
+    void elapsedSegmentTimeout();
+    void relayStateChanged(int, bool);
+    void startBrewing();
+    void updateBaseTemp(double);
+    void updateFloatTemp(double);
+    
+protected:
+    void showEvent(QShowEvent*) override;
+    
 private:
-    QScopedPointer<Ui::robobrew> m_ui;
+    void setPumpButtonBackground(bool);
+    void setInnerCoilButtonBackground(bool);
+    void setOuterCoilButtonBackground(bool);
+    
+    QGridLayout *m_layout;
+    QLCDNumber *m_temp1;
+    QLCDNumber *m_temp2;
+    QLabel *m_temp1Label;
+    QLabel *m_temp2Label;
+    QLCDNumber *m_timeElapsedLabel;
+    QLCDNumber *m_timeElapsedSegmentLabel;
+    QPushButton *m_innerCoil;
+    QPushButton *m_outerCoil;
+    QPushButton *m_pump;
+    QPushButton *m_starter;
+    RelayManager *m_relays;
+    QTimer *m_timerElapsedTime;
+    QTimer *m_timerElapsedSegmentTime;
+    
+    int m_timeElapsed;
+    int m_timeElapsedSegment;
 };
 
 #endif // ROBOBREW_H
